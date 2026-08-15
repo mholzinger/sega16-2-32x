51f5ff069b7c8640962609ca915a560b6a185a9e
Mike Holzinger <mike.holzinger@gmail.com>
2026-08-15 02:53:39 -0700

    [wip] m_main.c

--- sh_src/m_main.c
+++ sh_src/m_main.c
@@ -2435,6 +2435,7 @@ RAMCODE void m_main(void)
     FMT[0] = FMT[1] = FMT[2] = FMT[3] = 0;
     DRQR[0] = DRQR[1] = DRQR[2] = DRQR[3] = DRQR[4] = 0;
     DRQR[5] = DRQR[6] = 0;      /* drift split: small / catastrophic */
+    DRQR[7] = 0;                /* misaligned (FIFO word-loss) packets */
 #endif
 #ifdef WAIT_SPLIT_PROBE
     for (int i = 0; i < 9; i++)
