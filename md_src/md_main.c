8e1ab8aa1994a297a35f823df06b61684e1095f0
Mike Holzinger <mike.holzinger@gmail.com>
2026-09-12 16:07:38 -0400

    [wip] md_main.c

--- md_src/md_main.c
+++ md_src/md_main.c
@@ -639,6 +639,7 @@ static void md_consume(uint32_t pkt_base) {
 				{
 #ifdef TILE_VERIFY
 				uint16_t tv_fs0 = *(volatile uint16_t*)0xA1510A;
+				*(volatile uint16_t*)0xFFA1EC = sc[1];   /* SH-2 verdict bits 8-12 */
 #endif
 				(*(volatile uint16_t*)0xFFB0B2) =
 					*(volatile uint16_t*)0xC00008;
